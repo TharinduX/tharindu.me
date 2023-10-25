2a1fa7a7912a5e13ffa1a092b493a48d52fa5d55
Tharindu Jayasanka <hello@tharindu.me>
2023-10-25 20:58:00 +0530

    fix: typescript error.

--- sanity/schemas/project.ts
+++ sanity/schemas/project.ts
@@ -11,19 +11,19 @@ const project = {
       type: 'image',
       description: 'Upload a Project Icon or Logo',
       options: { hotspot: true },
-      validation: (Rule) => Rule.required(),
+      validation: (Rule: any) => Rule.required(),
     },
     defineField({
       name: 'projectName',
       title: 'Project Name',
       type: 'string',
-      validation: (Rule) => Rule.required(),
+      validation: (Rule: any) => Rule.required(),
     }),
     defineField({
       name: 'techStack',
       type: 'array',
       title: 'Tech stack',
-      validation: (Rule) => Rule.required(),
+      validation: (Rule: any) => Rule.required(),
       of: [
         defineArrayMember({
           type: 'object',
@@ -38,14 +38,14 @@ const project = {
       type: 'image',
       description: 'Upload a Project Screenshot',
       options: { hotspot: true },
-      validation: (Rule) => Rule.required(),
+      validation: (Rule: any) => Rule.required(),
     },
     {
       title: 'Project Category',
       name: 'projectCategory',
       type: 'string',
       initialValue: 'website',
-      validation: (Rule) => Rule.required(),
+      validation: (Rule: any) => Rule.required(),
       options: {
         list: [
           { title: 'Website', value: 'website' },
@@ -61,19 +61,19 @@ const project = {
       title: 'Project Description',
       type: 'text',
       rows: 4,
-      validation: (Rule) => Rule.required(),
+      validation: (Rule: any) => Rule.required(),
     },
     defineField({
       name: 'githubURL',
       title: 'GitHub URL',
       type: 'url',
-      validation: (Rule) => Rule.required(),
+      validation: (Rule: any) => Rule.required(),
     }),
     defineField({
       name: 'features',
       type: 'array',
       title: 'Features',
-      validation: (Rule) => Rule.required(),
+      validation: (Rule: any) => Rule.required(),
       of: [
         defineArrayMember({
           type: 'object',
@@ -89,7 +89,7 @@ const project = {
       title: 'Project Date',
       name: 'projectDate',
       type: 'date',
-      validation: (Rule) => Rule.required(),
+      validation: (Rule: any) => Rule.required(),
       options: {
         dateFormat: 'MMMM Do YYYY',
         calendarTodayLabel: 'Today',
